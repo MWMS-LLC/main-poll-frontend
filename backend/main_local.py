56955eb54a51e14c82b10c1e9c6be9019237bcea
MWMS-LLC <1withyin@gmail.com>
2025-08-17 16:55:40 -0400

    Fix CORS: Add local IP addresses for development

--- backend/main_local.py
+++ backend/main_local.py
@@ -36,6 +36,10 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:5173",
+        "http://localhost:5174",
+        "http://localhost:5175",
+        "http://192.168.87.244:5174",
+        "http://192.168.87.244:5175",
         "https://teen.myworldmysay.com",
         "https://myworldmysay.com"
     ],
